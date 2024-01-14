824ab9f66ddf03f2583298817a3e5bdfb81c7f60
ihfazhillah <mihfazhillah@gmail.com>
2024-01-14 23:48:58 +0700

    add chakra ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Box, Text} from "@chakra-ui/react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box w={"80%"} h="200px" bgGradient="linear(to-t, green.200, pink.500)" >
+          <Text fontWeight="extrabold" fontSize="6xl">
+              Hello world
+          </Text>
+        
+      </Box>
     </div>
   );
 }
